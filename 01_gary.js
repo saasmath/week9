839b2d188933cfb02406ff510408486b3e597b45
Gary Anderson <ganderson@seattleacademy>
2016-02-11 12:47:30 -0800

    improve 01 by adding third word

---
+++ 01_gary.js
@@ -0,0 +1,7 @@
+//Write a program that takes one argument
+//It should say "hello" and the argument
+//node 01_yourname.js world should output "Hello world"
+
+secondWord = process.argv[2];
+thirdWord = process.argv[3];
+console.log('Hello ' + secondWord + ' ' + thirdWord);
